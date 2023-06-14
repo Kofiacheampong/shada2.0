847843a2089bb0fe775bf20924dd92efb816f5ca
Kofiacheampong <acheampong0805@outlook.com>
2023-06-14 12:53:28 -0500

    added assets folder made changes to the home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Shada Trade',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'PermanentMarker',
-              color: Colors.grey,
-            ),
-          ),
-          centerTitle: false,
-          backgroundColor: Colors.white,
-        ),
-        body: const Center(
-          child: Text('Sharing is caring'),
-        ),
-      ),
+void main() => runApp(const MaterialApp(
+      home: Home(),
     ));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'SHADATRADE',
+          style: TextStyle(
+            fontSize: 20 ,
+            fontFamily: 'BalsamiqSans-Regular',
+            color: Colors.black,
+
+          ),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            Text('Welcome to ShadaTrade clothing exchange'
+            ),
+
+          ],
+        ),
+      )
+
+
+
+    );
+  }
+}
